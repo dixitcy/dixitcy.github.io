efc451f5a9e8cd61f543e9767c4f705eec59fbd3
Dixit Bavu <dixit@GetyourOwnWi-Fi.local>
2019-05-15 15:25:10 +0530

    Updates

---
+++ precache-manifest.1bc84fca2c677af320e4945e8d121db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cc8fe5f48c3e809f2aa726eae770d6",
+    "url": "/dixitcy.github.io/index.html"
+  },
+  {
+    "revision": "c2533cb6a332d4015631",
+    "url": "/dixitcy.github.io/static/css/main.35a1d5fd.chunk.css"
+  },
+  {
+    "revision": "0e288872990ca9e8eeab",
+    "url": "/dixitcy.github.io/static/js/2.c0eb0676.chunk.js"
+  },
+  {
+    "revision": "c2533cb6a332d4015631",
+    "url": "/dixitcy.github.io/static/js/main.715ca033.chunk.js"
+  },
+  {
+    "revision": "ae4ef6611dab25b91580",
+    "url": "/dixitcy.github.io/static/js/runtime~main.3ef2cc6f.js"
+  },
+  {
+    "revision": "ac5237052941a94686167d278e1c1c9d",
+    "url": "/dixitcy.github.io/static/media/SF-Pro-Display-Light.ac523705.otf"
+  },
+  {
+    "revision": "fa0b18d211e49491fb21f12c21aedd17",
+    "url": "/dixitcy.github.io/static/media/profilepic.fa0b18d2.png"
+  }
+]);
